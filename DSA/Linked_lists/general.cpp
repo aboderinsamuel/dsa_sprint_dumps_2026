ed730e50d27c9a89405b5cfca39f007cd8d511ff
Samuel Aboderin <aboderinseun01@gmail.com>
2026-09-22 16:10:00 +0100

    critical design LRU AND LFU CACHE

---
+++ DSA/Linked_lists/general.cpp
@@ -0,0 +1,226 @@
+//Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+//leetcode 25. Reverse Nodes in k-Group
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode dummy(0, head);
+
+        ListNode* prev = &dummy;
+        while(true){
+            //Find the kth node
+            ListNode* check  = prev;
+            for(int i=0; i < k; i++){
+                check = check->next;
+                if(!check){
+                    return dummy.next;
+                }
+            }
+
+            ListNode* curr = prev->next;
+            for(int i=0; i<k-1; i++){
+                ListNode* nxt = curr->next;
+                curr->next = nxt->next;
+                nxt->next = prev->next;
+                prev->next = nxt;
+            }
+            prev = curr;
+        }
+    }
+};
+
+// leetcode 92. Reverse Linked List II
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+
+        ListNode dummy(0, head);
+
+        ListNode* prev = &dummy;
+        for(int i=1; i<left; i++){
+            prev = prev->next;
+        }
+
+        ListNode* curr = prev->next;
+        for(int i=0; i<right-left; i++){
+            ListNode* nxt = curr->next;
+            curr->next = nxt->next;
+            nxt->next = prev->next;
+            prev->next = nxt;
+        }
+        return dummy.next;
+    }
+};
+//leetcode 206. Reverse Linked List
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+
+        while(curr){
+            ListNode* nxt = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nxt;
+        }
+        return prev;
+    }
+};
+
+//leetcode 622. Design Circular Queue
+#include <vector>
+using namespace std;
+class MyCircularQueue {
+private:
+    vector<int> data;
+    int head;
+    int count;
+    int cap;
+public:
+    MyCircularQueue(int k) : data(k), head(0), count(0), cap(k){
+        
+    }
+    
+    bool enQueue(int value) {
+        if(isFull()) return false;
+        data[(head+count)%cap] = value;
+        count++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if(isEmpty()) return false;
+        head = (head+1)%cap;
+        count--;
+        return true;
+    }
+    
+    int Front() {
+        return isEmpty() ? -1 : data[head];
+    }
+    
+    int Rear() {
+        return isEmpty() ? -1 : data[(head+count-1)%cap];
+    }
+    
+    bool isEmpty() {
+        return count == 0;
+    }
+    
+    bool isFull() {
+        return count == cap;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
+////or.....
+
+// leetcode 460 : LFU Cache
+#include <unordered_map>
+#include <list>
+using namespace std;
+
+class LFUCache{
+private:
+    struct Entry{
+        int value;
+        int freq;
+        list<int>::iterator it; // pointer to the position of the key in the frequency list
+    };
+
+    int cap;
+    int minFreq;
+    unordered_map<int, Entry> nodes; // key to Entry mapping
+    unordered_map<int, list<int>> freqList; // frequency to list of keys mapping
+
+    void touch(int key){
+        Entry& e = nodes[key]; // get the entry for the key
+        freqList[e.freq].erase(e.it); // remove the key from its current frequency list
+        if(freqList[e.freq].empty()){
+            freqList.erase(e.freq); // remove the frequency list if it's empty
+            if(minFreq == e.freq) minFreq++; // update minFreq if needed
+        }
+        e.freq++; // increment the frequency
+        freqList[e.freq].push_front(key); // add the key to the front of the new frequency list
+        e.it = freqList[e.freq].begin(); // update the iterator to the new position
+    }
+
+    LFUCache(int capacity) : cap(capacity), minFreq(0) {}
+
+    int get(int key){
+        if(!nodes.count(key)) return -1; // key not found
+        touch(key); // update the frequency of the key
+        return nodes[key].value; // return the value
+    }
+    void put(int key, int value){
+        if(cap <= 0) return; // no capacity
+        if(nodes.count(key)){
+            nodes[key].value = value; // update the value
+            touch(key); // update the frequency
+            return;
+        }
+        if((int)nodes.size() >= cap){
+            int evict = freqList[minFreq].back(); // get the least frequently used key
+            freqList[minFreq].pop_back(); // remove it from the frequency list
+            if(freqList[minFreq].empty()) freqList.erase(minFreq); // remove the frequency list if it's empty
+            nodes.erase(evict); // remove the key from the nodes map
+        }
+        freqList[1].push_front(key); // add the new key to the frequency list for frequency 1
+        nodes[key] = {value, 1, freqList[1].begin()}; // add the new key with frequency 1
+        minFreq = 1; // reset minFreq to 1
+    }
+
+};
+
+// leetcode 146 : LRU Cache
+class LRUCache {
+private:
+    int cap;
+    list<pair<int, int>> cache; // list of key-value pairs
+    unordered_map<int, list<pair<int, int>>::iterator> pos; // key to iterator mapping
+
+public:
+    LRUCache(int capacity) : cap(capacity) {}
+
+    int get(int key) {
+        auto it = pos.find(key);
+        if(it == pos.end()) return -1; // key not found
+        cache.splice(cache.begin(), cache, it->second); // move the accessed key-value pair
+        return it->second->second; // return the value
+    }
+
+    void put(int key, int value) {
+        auto it = pos.find(key);
+        if(it != pos.end()){
+            it->second->second = value; // update the value
+            cache.splice(cache.begin(), cache, it->second); // move the existing key-value pair to the front
+            return;
+        }
+        if((int)cache.size() == cap){
+            pos.erase(cache.back().first); // remove the least recently used key from the map
+            cache.pop_back(); // remove the least recently used key-value pair from the list
+        }
+        cache.emplace_front(key, value); // add the new key-value pair to the front
+        pos[key] = cache.begin(); // update the map with the new iterator
+    }
+};
+
+
+
--- DSA/randoms/bs.cpp
+++ DSA/randoms/bs.cpp
@@ -8,35 +8,35 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if(nums1.size() > nums2.size()){
-            return findMedianSortedArrays(nums2, nums1);
+            return findMedianSortedArrays(nums2, nums1); // always make sure nums1 is the smaller array to optimize the binary search
         }
         int m = nums1.size();
         int n = nums2.size();
 
-        int left = 0;
-        int right = m;
+        int left = 0; // left boundary of the search space
+        int right = m; // right boundary of the search space
         while(left <= right){
             int i = left + (right-left)/2;
             // i + j = (m+n+!)/2
-            int j = ((m + n +1) /2) - i;
+            int j = ((m + n +1) /2) - i; // j is the index in nums2 that complements i to partition the combined arrays into two halves
             
-            int nums1Left = (i == 0) ? INT_MIN : nums1[i-1];
-            int nums1Right = (i == m) ? INT_MAX : nums1[i];
-            int nums2Left = (j == 0) ? INT_MIN : nums2[j-1];
-            int nums2Right = (j == n) ? INT_MAX : nums2[j];
+            int nums1Left = (i == 0) ? INT_MIN : nums1[i-1]; // if i is 0, it means there are no elements on the left side of nums1, so we use INT_MIN to represent that
+            int nums1Right = (i == m) ? INT_MAX : nums1[i]; // if i is m, it means there are no elements on the right side of nums1, so we use INT_MAX to represent that
+            int nums2Left = (j == 0) ? INT_MIN : nums2[j-1]; // if j is 0, it means there are no elements on the left side of nums2, so we use INT_MIN to represent that
+            int nums2Right = (j == n) ? INT_MAX : nums2[j]; // if j is n, it means there are no elements on the right side of nums2, so we use INT_MAX to represent that
 
-            if(nums1Left <= nums2Right && nums2Left <= nums1Right){
-                if((m+n)% 2 == 0){
-                    int leftMax = max(nums1Left, nums2Left);
-                    int rightMin = min(nums1Right, nums2Right);
-                    return (leftMax + rightMin) / 2.0;
+            if(nums1Left <= nums2Right && nums2Left <= nums1Right){ // if the largest element on the left side of nums1 is less than or equal to the smallest element on the right side of nums2, and the largest element on the left side of nums2 is less than or equal to the smallest element on the right side of nums1, then we have found the correct partition
+                if((m+n)% 2 == 0){ // if the total number of elements is even, the median is the average of the two middle elements
+                    int leftMax = max(nums1Left, nums2Left); // the largest element on the left side of the partition
+                    int rightMin = min(nums1Right, nums2Right); // the smallest element on the right side of the partition
+                    return (leftMax + rightMin) / 2.0; // the median is the average of the two middle elements
                 }else{
-                    return max(nums1Left, nums2Left);
+                    return max(nums1Left, nums2Left); // if the total number of elements is odd, the median is the largest element on the left side of the partition
                 }
-            }else if(nums1Left > nums2Right){
-                right = i-1;
+            }else if(nums1Left > nums2Right){ // if the largest element on the left side of nums1 is greater than the smallest element on the right side of nums2, it means we need to move the partition in nums1 to the left
+                right = i-1; // move the right boundary of the search space to the left
             }else{
-                left = i+ 1;
+                left = i+ 1; // if the largest element on the left side of nums2 is greater than the smallest element on the right side of nums1, it means we need to move the partition in nums1 to the right
             }
         }
         return 0.0;
