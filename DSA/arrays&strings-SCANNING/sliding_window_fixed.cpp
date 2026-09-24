df43613a11fbb558dc0378d2543912aec00a2246
Samuel Aboderin <aboderinseun01@gmail.com>
2026-09-24 17:10:46 +0100

    some changes

--- DSA/arrays&strings-SCANNING/sliding_window_fixed.cpp
+++ DSA/arrays&strings-SCANNING/sliding_window_fixed.cpp
@@ -9,8 +9,10 @@ public:
         int left = 0;
         int right = s.size() - 1;
         while(left < right){
-            while(left < right && !vowels.count(s[left])) left++; // Move left pointer to the next vowel
-            while(left < right && !vowels.count(s[right])) right--; // Move right pointer to the previous vowel
+            // If the character at the left pointer is not a vowel, we keep moving the left pointer to the right until we find a vowel or the pointers cross
+            while(left < right && !vowels.count(s[left])) left++;
+            // If the character at the right pointer is not a vowel, we keep moving the right pointer to the left until we find a vowel or the pointers cross
+            while(left < right && !vowels.count(s[right])) right--;
             swap(s[left], s[right]); // Swap the vowels
             left++;
             right--;
--- bloomberg_docs/number_of_ships_in_rectangle.cpp
+++ bloomberg_docs/number_of_ships_in_rectangle.cpp
@@ -34,3 +34,28 @@ public:
 //time : O(nlogn) where n is the number of ships in the rectangle. The recursive function divides the rectangle into four quadrants at each level, 
 //and the depth of the recursion is logarithmic with respect to the size of the rectangle.
 // space : O(logn) for the recursion stack.
+
+class Solution {
+public:
+    int countShips(Sea sea, vector<int> topRight, vector<int> bottomLeft) {
+        int x1 = bottomLeft[0], y1 = bottomLeft[1]; // bottom-left corner of the rectangle
+        int x2 = topRight[0],   y2 = topRight[1]; // top-right corner of the rectangle
+
+        if (x1 > x2 || y1 > y2) return 0; // invalid rectangle
+
+        if (!sea.hasShips(topRight, bottomLeft)) return 0; // no ships in the rectangle
+
+        if (x1 == x2 && y1 == y2) return 1; // the rectangle is a single point and has a ship
+
+        int mx = x1 + (x2 - x1) / 2; // mid-point x-coordinate
+        int my = y1 + (y2 - y1) / 2; // mid-point y-coordinate
+
+        return countShips(sea, {mx, my},{x1,y1}) // bottom-left quadrant
+             + countShips(sea, {x2, my},{mx + 1, y1}) // bottom-right quadrant 
+             + countShips(sea, {mx, y2}, {x1, my + 1}) // top-left quadrant
+             + countShips(sea, {x2, y2}, {mx + 1, my + 1}); // top-right quadrant
+    } 
+};
+//for this : time : O(nlogn) where n is the number of ships in the rectangle. The recursive function divides the rectangle into four quadrants at each level,
+// and the depth of the recursion is logarithmic with respect to the size of the rectangle.
+// space : O(logn) for the recursion stack.
